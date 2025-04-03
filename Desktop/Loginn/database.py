01cabed7463da8a21dd89690930fbef4e274b2b7
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-04-03 09:34:20 +0200

    a function to fetch employees

--- Desktop/Loginn/database.py
+++ Desktop/Loginn/database.py
@@ -12,3 +12,18 @@ def create_databases():
     
     conn.commit()
     conn.close()
+    
+def add_employee(name, fingerprint_dat):
+    conn = sqlite3.connect('employees.db')
+    cursor = conn.cursor()
+    
+    cursor.execute("INSERT INTO employee (name, fingerprint)VALUES (?, ?)", name, fingerprint_dat)
+    
+    conn.commit()
+    conn.close()
+    
+    #function to fetch all the employees
+    
+def fetch_employees():
+    conn = sqlite3.connect('employees.db')
+    cursor = conn.cursor
