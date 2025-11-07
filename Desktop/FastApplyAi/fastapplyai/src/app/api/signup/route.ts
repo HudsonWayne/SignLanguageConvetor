c3c6f8c206435daaccd547238675d19a1e60b1b7
HudsonWayne <waynewaynebenhura@gmail.com>
2025-11-07 12:22:01 +0200

    initial commit

---
+++ Desktop/FastApplyAi/fastapplyai/src/app/api/signup/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "@/lib/mongodb";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, password } = await req.json();
+    await connectDB();
+
+    // check if exists
+    const exists = await User.findOne({ email });
+    if (exists) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    await User.create({ name, email, password: hashed });
+
+    return NextResponse.json(
+      { message: "User registered!" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Server error" },
+      { status: 500 }
+    );
+  }
+}
