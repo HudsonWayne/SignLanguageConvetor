4011a2f7da7f0fba6018277be9daffdef49327c2
HudsonWayne <waynewaynebenhura@gmail.com>
2025-11-11 09:49:14 +0200

    initial commit

--- Desktop/FastApplyAi/fastapplyai/src/lib/mongoClient.ts
+++ Desktop/FastApplyAi/fastapplyai/src/lib/mongoClient.ts
@@ -1,24 +1,21 @@
+// /lib/mongoClient.ts
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI!;
 const options = {};
 
-let client;
+if (!uri) throw new Error("Please define the MONGODB_URI environment variable.");
+
+let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable.");
-}
-
 if (process.env.NODE_ENV === "development") {
-  // In development, use a global variable to preserve the connection across hot reloads
   if (!(global as any)._mongoClientPromise) {
     client = new MongoClient(uri, options);
     (global as any)._mongoClientPromise = client.connect();
   }
   clientPromise = (global as any)._mongoClientPromise;
 } else {
-  // In production, it's fine to create a new client
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
