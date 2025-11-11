a9db851fb22b653cf37a50885ae0c7af4c20136c
HudsonWayne <waynewaynebenhura@gmail.com>
2025-11-11 09:50:11 +0200

    initial commit

--- Desktop/FastApplyAi/fastapplyai/src/lib/User.ts
+++ Desktop/FastApplyAi/fastapplyai/src/lib/User.ts
@@ -1,16 +1,24 @@
+// /models/user.ts
 import mongoose, { Schema, Document, Model } from "mongoose";
 
 export interface IUser extends Document {
   name: string;
   email: string;
+  password?: string;
   image?: string;
 }
 
-const UserSchema: Schema<IUser> = new Schema({
-  name: { type: String, required: false },
-  email: { type: String, required: true, unique: true },
-  image: { type: String },
-});
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    name: { type: String, required: false },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    image: { type: String },
+  },
+  { timestamps: true }
+);
 
-const User: Model<IUser> = mongoose.models.User || mongoose.model("User", UserSchema);
+const User: Model<IUser> =
+  mongoose.models.User || mongoose.model("User", UserSchema);
+
 export default User;
