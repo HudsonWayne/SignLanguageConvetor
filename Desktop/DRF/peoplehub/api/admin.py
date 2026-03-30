f89ba98b2dfc581056f826d0011bd716b39c75b6
HudsonWayne <waynewaynebenhura@gmail.com>
2026-03-30 14:30:38 +0200

    initial commit

--- Desktop/DRF/peoplehub/api/admin.py
+++ Desktop/DRF/peoplehub/api/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import Person
 
-# Register your models here.
+
 @admin.register(Person)
-
-
 class PersonAdmin(admin.ModelAdmin):
-    list_display = "_all_"
+    list_display = ['name', 'city', 'age']
