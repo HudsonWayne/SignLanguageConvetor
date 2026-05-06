8430c8215c18f275bedecd841f110481f2bf4fc1
HudsonWayne <waynewaynebenhura@gmail.com>
2026-05-06 09:48:49 +0200

    initial commit

--- Desktop/DerivCRT-main/main.py
+++ Desktop/DerivCRT-main/main.py
@@ -1,18 +1,22 @@
 from deriv_ws import DerivLiveStreamer
 from live_plot import LiveCandlePlot
 from pattern_detector import CRTStrategy
-import pandas as pd
 
 symbol = "R_75"
+
 candles = []
 signals = []
 
+strategy = CRTStrategy(candles)
+
 def on_new_candle(candle):
     global signals
+
     candles.append(candle)
-    if len(candles) > 100:
+
+    if len(candles) > 200:
         candles.pop(0)
-    strategy = CRTStrategy(candles)
+
     signals = strategy.run()
 
 def get_candles():
@@ -22,7 +26,14 @@ def get_signals():
     return signals
 
 if __name__ == "__main__":
-    print("🟢 Starting CRT + Breakout Detector with Thank You Chart")
-    DerivLiveStreamer(app_id="1089", symbol=symbol, granularity=14400, callback=on_new_candle).start()
+    print("🚀 SMART MONEY ENGINE STARTED")
+
+    DerivLiveStreamer(
+        app_id="1089",
+        symbol=symbol,
+        granularity=14400,
+        callback=on_new_candle
+    ).start()
+
     plot = LiveCandlePlot(get_candles, get_signals)
     plot.show()
